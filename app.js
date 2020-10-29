0b3a8c85eac17765a2c9f91b4eb77a0ec4388ade
hunjin_chung <hunjin_chung@tamx.co,kr>
2020-10-29 20:08:59 +0900

    [feat] routes and controllers

---
+++ app.js
@@ -0,0 +1,26 @@
+import "core-js";
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import userRouter from "./routers/userRouter";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import routes from "./routes";
+var app = express();
+
+// 서버에서 읽을 수 있게 parse
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:true }));
+// 보안
+app.use(helmet());
+// 로그
+app.use(morgan("dev"));
+
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
+
+export default app;

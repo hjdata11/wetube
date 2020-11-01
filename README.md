92fbe0c3d64ab2bfc62bee2e4158c0f9e9884a6b
unknown <hjdata11@naver.com>
2020-11-01 22:44:25 +0900

    [feat] Video controllers and views

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # WETUBE
 
 Cloning Youtube with VanillaJS and NodeJS
+
+## Pages:
+
+- [ ] Home
+- [X] Join
+- [X] Login
+- [x] Search
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Change Password
+- [ ] Upload
+- [ ] Video Detail
+- [ ] Edit Video
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,14 +1,21 @@
 import express from "express";
-import { join, login, logout } from "../controller/userController";
-import { home, search } from "../controller/videoControllers";
 import routes from "../routes";
-
+import { home, search } from "../controller/videoController";
+import { getJoin, postJoin, getLogin, postLogin, logout} from "../controller/userController";
+  
+  
 const globalRouter = express.Router();
+  
+  
+globalRouter.get(routes.join, getJoin);
+globalRouter.post(routes.join, postJoin);
+  
+globalRouter.get(routes.login, getLogin);
+globalRouter.post(routes.login, postLogin);
 
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
-globalRouter.get(routes.join, join);
-globalRouter.get(routes.login, login);
+
 globalRouter.get(routes.logout, logout);
 
 
